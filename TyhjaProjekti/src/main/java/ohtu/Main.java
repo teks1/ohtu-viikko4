2105f5cd5d8c24b2e970d4ba24f07eca06abe9f2
teks1 <marko.vuorenmaa@cs.helsinki.fi>
2016-04-13 21:16:25 +0300

    Neljäs viikko alku

--- TyhjaProjekti/src/main/java/ohtu/Main.java
+++ TyhjaProjekti/src/main/java/ohtu/Main.java
@@ -1,7 +1,50 @@
 package ohtu;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import java.io.IOException;
+import java.io.InputStream;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.io.IOUtils;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        String studentNr = "014438341";
+        if (args.length > 0) {
+            studentNr = args[0];
+        }
+        String url = "http://ohtustats2016.herokuapp.com/students/" + studentNr + "/submissions";
+
+        HttpClient client = new HttpClient();
+        GetMethod method = new GetMethod(url);
+        client.executeMethod(method);
+
+        InputStream stream = method.getResponseBodyAsStream();
+
+        String bodyText = IOUtils.toString(stream);
+
+//        System.out.println("json-muotoinen data:");
+        System.out.println(bodyText);
+        Gson mapper = new Gson();
+        Submission[] subs = mapper.fromJson(bodyText, Submission[].class);
+
+//        System.out.println("Oliot:");
+        int tehtavatYhteensa = 0;
+        int aikaYhteensa = 0;
+        System.out.println("opiskelijanumero " + studentNr + "\n");
+        for (Submission sub : subs) {
+            aikaYhteensa += sub.getHours();
+            tehtavatYhteensa += sub.getTotalExercises();
+            System.out.println(sub);
+        }
+
+        System.out.println("\nyhteensä: " + tehtavatYhteensa + " tehtävää " + aikaYhteensa + " tuntia");
+
+        
+        String lol = "";
+        System.out.println(lol);
     }
 }
--- TyhjaProjekti/src/main/java/ohtu/Submission.java
+++ TyhjaProjekti/src/main/java/ohtu/Submission.java
@@ -1,7 +1,93 @@
 package ohtu;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Submission {
+
     private String student_number;
+    private int week;
+    private int hours;
+    private boolean a1 = false;
+    private boolean a2 = false;
+    private boolean a3 = false;
+    private boolean a4 = false;
+    private boolean a5 = false;
+    private boolean a6 = false;
+    private boolean a7 = false;
+    private boolean a8 = false;
+    private boolean a9 = false;
+    private boolean a10 = false;
+    private boolean a11 = false;
+    private boolean a12 = false;
+    private String doneExercises = "";
+    private int totalExercises = 0;
+
+    public int getTotalExercises() {
+        if (a1) {
+            totalExercises++;
+            doneExercises += "1 ";
+            a1 = false;
+        }
+        if (a2) {
+            totalExercises++;
+            doneExercises += "2 ";
+            a2 = false;
+        }
+        if (a3) {
+            totalExercises++;
+            doneExercises += "3 ";
+            a3 = false;
+        }
+        if (a4) {
+            totalExercises++;
+            doneExercises += "4 ";
+            a4 = false;
+        }
+        if (a5) {
+            totalExercises++;
+            doneExercises += "5 ";
+            a5 = false;
+        }
+        if (a6) {
+            totalExercises++;
+            doneExercises += "6 ";
+            a6 = false;
+        }
+        if (a7) {
+            totalExercises++;
+            doneExercises += "7 ";
+            a7 = false;
+        }
+        if (a8) {
+            totalExercises++;
+            doneExercises += "8 ";
+            a8 = false;
+        }
+        if (a9) {
+            totalExercises++;
+            doneExercises += "9 ";
+            a9 = false;
+        }
+        if (a10) {
+            totalExercises++;
+            doneExercises += "10 ";
+            a10 = false;
+        }
+        if (a11) {
+            totalExercises++;
+            doneExercises += "11 ";
+            a11 = false;
+        }
+        if (a12) {
+            totalExercises++;
+            doneExercises += "12 ";
+            a12 = false;
+        }
+
+        return totalExercises;
+    }
 
     public String getStudent_number() {
         return student_number;
@@ -13,7 +99,121 @@ public class Submission {
 
     @Override
     public String toString() {
-        return student_number;
+        return "viikko " + week + ": tehtyjä tehtäviä yhteensä: " + getTotalExercises() + ", aikaa kului "
+                + hours + " tuntia, tehdyt tehtävät:" + doneExercises;
     }
-    
+
+    public int getWeek() {
+        return week;
+    }
+
+    public void setWeek(int week) {
+        this.week = week;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public Boolean isA1() {
+        return a1;
+    }
+
+    public void setA1(Boolean a1) {
+        this.a1 = a1;
+        System.out.println(a1);
+    }
+
+    public boolean isA2() {
+        return a2;
+    }
+
+    public void setA2(boolean a2) {
+        this.a2 = a2;
+    }
+
+    public boolean isA3() {
+        return a3;
+    }
+
+    public void setA3(boolean a3) {
+        this.a3 = a3;
+    }
+
+    public boolean isA4() {
+        return a4;
+    }
+
+    public void setA4(boolean a4) {
+        this.a4 = a4;
+    }
+
+    public boolean isA5() {
+        return a5;
+    }
+
+    public void setA5(boolean a5) {
+        this.a5 = a5;
+    }
+
+    public boolean isA6() {
+        return a6;
+    }
+
+    public void setA6(boolean a6) {
+        this.a6 = a6;
+    }
+
+    public boolean isA7() {
+        return a7;
+    }
+
+    public void setA7(boolean a7) {
+        this.a7 = a7;
+    }
+
+    public boolean isA8() {
+        return a8;
+    }
+
+    public void setA8(boolean a8) {
+        this.a8 = a8;
+    }
+
+    public boolean isA9() {
+        return a9;
+    }
+
+    public void setA9(boolean a9) {
+        this.a9 = a9;
+    }
+
+    public boolean isA10() {
+        return a10;
+    }
+
+    public void setA10(boolean a10) {
+        this.a10 = a10;
+    }
+
+    public boolean isA11() {
+        return a11;
+    }
+
+    public void setA11(boolean a11) {
+        this.a11 = a11;
+    }
+
+    public boolean isA12() {
+        return a12;
+    }
+
+    public void setA12(boolean a12) {
+        this.a12 = a12;
+    }
+
 }

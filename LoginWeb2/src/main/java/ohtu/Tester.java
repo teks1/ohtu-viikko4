ecb8ba4841b318f4362963ddbf0bc39902708bb8
teks1 <marko.vuorenmaa@cs.helsinki.fi>
2016-04-13 22:30:43 +0300

    Viikko 4, teht. 8

--- LoginWeb2/src/main/java/ohtu/Tester.java
+++ LoginWeb2/src/main/java/ohtu/Tester.java
@@ -3,29 +3,66 @@ package ohtu;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 public class Tester {
+
     public static void main(String[] args) {
-        WebDriver driver = new HtmlUnitDriver();
+        WebDriver driver = new FirefoxDriver();
 
         driver.get("http://localhost:8090");
-        System.out.println( driver.getPageSource() );
-        WebElement element = driver.findElement(By.linkText("login"));       
-        element.click(); 
+//        System.out.println(driver.getPageSource());
+//        WebElement element = driver.findElement(By.linkText("login"));
+//        element.click();
+//
+//        System.out.println("==");
+//
+//        System.out.println(driver.getPageSource());
+//        element = driver.findElement(By.name("username"));
+//        element.sendKeys("pekka");
+//        element = driver.findElement(By.name("password"));
+//        element.sendKeys("akkep");
+//        element = driver.findElement(By.name("login"));
+//        element.submit();
+//
+//        System.out.println("==");
+//        System.out.println(driver.getPageSource());
+
+//        WebElement element = driver.findElement(By.linkText("login"));
+//        element.click();
+//        element = driver.findElement(By.name("username"));
+//        element.sendKeys("pekka");
+//        element = driver.findElement(By.name("password"));
+//        element.sendKeys("lol");
+//        element = driver.findElement(By.name("login"));
+//        element.submit();
+//        System.out.println(driver.getPageSource());
+//        WebElement element = driver.findElement(By.linkText("login"));
+//        element.click();
+//        element = driver.findElement(By.name("username"));
+//        element.sendKeys("user");
+//        element = driver.findElement(By.name("password"));
+//        element.sendKeys("akkep");
+//        element = driver.findElement(By.name("login"));
+//        element.submit();
+//        System.out.println(driver.getPageSource());
         
-        System.out.println("==");
-        
-        System.out.println( driver.getPageSource() );
+        WebElement element = driver.findElement(By.linkText("register new user"));
+        element.click();
         element = driver.findElement(By.name("username"));
-        element.sendKeys("pekka");
+        element.sendKeys("Pentti");
         element = driver.findElement(By.name("password"));
-        element.sendKeys("akkep");
-        element = driver.findElement(By.name("login"));
+        element.sendKeys("1234567a");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("1234567a");
+        element = driver.findElement(By.name("add"));
         element.submit();
+        System.out.println(driver.getPageSource());
         
-        System.out.println("==");
-        System.out.println( driver.getPageSource() );
-        
+        element = driver.findElement(By.partialLinkText("continue"));
+        element.click();
+        element = driver.findElement(By.linkText("logout"));
+        element.click();
     }
 }
